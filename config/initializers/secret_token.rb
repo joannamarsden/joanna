08ad01bef956c2cd194e299c58f332a7304d57cf
Joanna Marsden <joanna.marsden@gmail.com>
2014-04-22 19:31:21 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joanna::Application.config.secret_token = '805890308be4deae1a4b6b6ad077d50d3cce1e5cc112a1fe48fd7fb1d2102660059e3a55e4350f2be978a5efeb0bf9aa3dca627d40685814de73f3f440774464'
